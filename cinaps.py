696b156904653d26f1b1441181ed3b65cc5c9b41
Biéchy <88650108+Biechy@users.noreply.github.com>
2023-01-22 15:44:32 +0100

    Code Lucas pour tout les models

--- cinaps.py
+++ cinaps.py
@@ -7,6 +7,8 @@ from torchvision.transforms import ToTensor, Lambda
 #from pytorch_model_summary import summary
 import numpy as np
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -15,28 +17,21 @@ training_data = datasets.CIFAR10(
     root="data",
     train=True,
     download=True,
-    transform=ToTensor()
+    transform=transform
 )
 
 test_data = datasets.CIFAR10(
     root="data",
     train=False,
     download=True,
-    transform=ToTensor()
+    transform=transform
 )
 
 batch_size = 32
 train = DataLoader(training_data, batch_size=batch_size, shuffle=True)
 test = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-print("Image shape of a random sample image : {}".format(
-    training_data[0][0].numpy().shape), end='\n\n')
-print("Training Set:   {} images".format(len(training_data)))
-print("Test Set:       {} images".format(len(test_data)))
-
-# need to return an tensor in option A
-
-
+# needed to return an tensor in option A
 class Identity(nn.Module):
 
     def __init__(self, lambd):
@@ -46,16 +41,14 @@ class Identity(nn.Module):
     def forward(self, x):
         return self.lambd(x)
 
-# will be use as block_type ne
-
-
+# will be use as block_type next
 class ConvBlock(nn.Module):
     '''
     ConvBlock will implement the regular ConvBlock and the shortcut block. See figure 2
     When the dimension changes between 2 blocks, the option A or B of the paper are available.
     '''
 
-    def __init__(self, in_channels, out_channels, stride=1, option='A'):
+    def __init__(self, in_channels, out_channels, stride=1, option='A', ResNet = True):
         super(ConvBlock, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3,
@@ -83,15 +76,18 @@ class ConvBlock(nn.Module):
                     lambda x: nn.functional.pad(x[:, :, ::2, ::2], padding))
             if option == 'B':
                 self.shortcut = nn.Sequential(
-                    nn.Conv2d(in_channels, 2*out_channels, kernel_size=1,
+                    nn.Conv2d(in_channels, out_channels, kernel_size=1,
                               stride=stride, padding=0, bias=False),
-                    nn.BatchNorm2d(2*out_channels)
+                    nn.BatchNorm2d(out_channels)
                 )
+        
+        self.ResNet = ResNet
 
     def forward(self, x):
         out = self.features(x)
         # sum it up with shortcut layer
-        out += self.shortcut(x)
+        if self.ResNet:
+            out += self.shortcut(x)
         out = self.relu(out)
         return out
 
@@ -101,7 +97,7 @@ class ResNetNN(nn.Module):
     ResNet-56 architecture for CIFAR-10 DataSet of shape 32*32*3
     """
 
-    def __init__(self, block_type, num_blocks):
+    def __init__(self, block_type, num_blocks, option, ResNet):
         super(ResNetNN, self).__init__()
 
         self.in_channels = 16  # soit c'est filters soit c'est feature maps
@@ -110,24 +106,24 @@ class ResNetNN(nn.Module):
                                stride=1, padding=1, bias=False)
         self.bn0 = nn.BatchNorm2d(16)
         self.block1 = self.__build_layer(
-            block_type, 16, num_blocks[0], starting_stride=1)
+            block_type, 16, num_blocks[0], starting_stride=1, option=option, ResNet=ResNet)
         self.block2 = self.__build_layer(
-            block_type, 32, num_blocks[1], starting_stride=2)
+            block_type, 32, num_blocks[1], starting_stride=2, option=option, ResNet=ResNet)
         self.block3 = self.__build_layer(
-            block_type, 64, num_blocks[2], starting_stride=2)
+            block_type, 64, num_blocks[2], starting_stride=2, option=option, ResNet=ResNet)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # jsp à quoi ca sert
         self.relu = nn.ReLU()
         self.linear = nn.Linear(64, 10)  # final activation for classification
 
-    def __build_layer(self, block_type, out_channels, num_blocks, starting_stride):
+    def __build_layer(self, block_type, out_channels, num_blocks, starting_stride, option, ResNet):
         # create a list of len num_blocks with the first the stride we want then follow by ones
         strides_list_for_current_block = [starting_stride]+[1]*(num_blocks-1)
 
         # boucle for to create a mutiple layer with de good in_channels and out_channels and the good stride defined above
         layers = []
         for stride in strides_list_for_current_block:
-            layers.append(block_type(self.in_channels, out_channels, stride))
+            layers.append(block_type(self.in_channels, out_channels, stride, option, ResNet))
             self.in_channels = out_channels
 
         return nn.Sequential(*layers)
@@ -145,54 +141,51 @@ class ResNetNN(nn.Module):
         return out
 
 
-def ResNet56():
-    return ResNetNN(block_type=ConvBlock, num_blocks=[9, 9, 9])
+def ResNet56A():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[9, 9, 9], option='A', ResNet=True)
 
+def ResNet44A():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[7,7,7], option='A', ResNet=True)
 
-model = ResNet56()
+def ResNet32A():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[5,5,5], option='A', ResNet=True)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+def ResNet20A():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[3,3,3], option='A', ResNet=True)
 
+def ResNet56B():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[9, 9, 9], option='B', ResNet=True)
 
-def train_loop(dataloader, model, loss_fn, optimizer, prin=False):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X)
-        loss = loss_fn(pred, y)
+def ResNet44B():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[7,7,7], option='B', ResNet=True)
 
-        # Backpropagation (always in three steps)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+def ResNet32B():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[5,5,5], option='B', ResNet=True)
 
-        if batch % 100 == 0 and prin:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+def ResNet20B():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[3,3,3], option='B', ResNet=True)
 
+def CNN56():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[9, 9, 9], option='A', ResNet=False)
 
-def test_loop(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss, correct = 0, 0
+def CNN44():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[7,7,7], option='A', ResNet=False)
 
-    with torch.no_grad():
-        for X, y in dataloader:
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+def CNN32():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[5,5,5], option='A', ResNet=False)
 
-    test_loss /= num_batches
-    correct /= size
-    print(
-        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}")
+def CNN20():
+    return ResNetNN(block_type=ConvBlock, num_blocks=[3,3,3], option='A', ResNet=False)
 
+models = [ResNet56A(), ResNet44A(), ResNet32A(), ResNet20A(), ResNet56B(), ResNet44B(), ResNet32B(),ResNet20B(), CNN56(), CNN44(), CNN32(),CNN20()]
+models_str = ["ResNet56A", "ResNet44A", "ResNet32A", "ResNet20A", "ResNet56B", "ResNet44B", "ResNet32B","ResNet20B", "CNN56", "CNN44", "CNN32","CNN20"]
 
-def train_model():
-    EPOCHS = 15
+def train_model(model, model_name):
+    EPOCHS = 10
     train_samples_num = 50000
     train_costs, val_costs = [], []
+
+    print(model_name)
 
     # Training phase.
     for epoch in range(EPOCHS):
@@ -201,6 +194,9 @@ def train_model():
         correct_train = 0
 
         for inputs, labels in train:
+
+            inputs.to(device)
+            labels.to(device)
 
             """ for every mini-batch during the training phase, we typically want to explicitly set the gradients 
             to zero before starting to do backpropragation """
@@ -255,33 +251,24 @@ def train_model():
         print('Test accuracy: {}'.format(test_accuracy))
 
         string = str(test_accuracy) + ", "
+        fichier = open("{}.txt".format(model_name), "w")
         fichier.write(string)
+        fichier.close()
+        torch.save(model.state_dict(), './models/{}'.format(model_name))
+
 
         print(info.format(epoch+1, EPOCHS, train_epoch_loss, train_acc))
+
+
 
     return train_costs
 
 
-fichier = open("error.txt", "x")
+for model, model_name in zip(models, models_str):
 
-train_costs = train_model()
+    model.to(device)
 
-fichier.close()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-test_samples_num = 10000
-correct = 0
-
-
-with torch.no_grad():
-    for inputs, labels in test:
-        # Make predictions.
-        prediction = model(inputs)
-
-        # Retrieve predictions indexes.
-        _, predicted_class = torch.max(prediction.data, 1)
-
-        # Compute number of correct predictions.
-        correct += (predicted_class == labels).float().sum().item()
-
-test_accuracy = correct / test_samples_num
-print('Test accuracy: {}'.format(test_accuracy))
+    train_costs = train_model(model, model_name)
